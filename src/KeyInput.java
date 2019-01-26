1cd50870d3645251690c095b2d2339cdc8d6dd0f
Khang <phan.khang5@gmail.com>
2019-01-26 00:25:52 -0500

    Added player boxes keyboard events and mouse pressed events

---
+++ src/KeyInput.java
@@ -0,0 +1,90 @@
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class KeyInput extends KeyAdapter {
+    private Handler handler;
+
+    public KeyInput(Handler handler){
+        this.handler = handler;
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int key = e.getKeyCode();
+//        System.out.println(key);
+//        super.keyPressed(e);
+        for (int i = 0; i < handler.objects.size(); i++){
+            GameObject tempObject = handler.objects.get(i);
+
+            if(tempObject.getId() == ID.Player){
+                // key event for player1
+                if(key == KeyEvent.VK_W) {
+                    tempObject.setVelY(-5);
+                }
+                if(key == KeyEvent.VK_A) {
+                    tempObject.setVelX(-5);
+                }
+                if(key == KeyEvent.VK_S) {
+                    tempObject.setVelY(5);
+                }
+                if(key == KeyEvent.VK_D) {
+                    tempObject.setVelX(5);
+                }
+            }
+            if(tempObject.getId() == ID.Player2){
+                if(key == KeyEvent.VK_UP) {
+                    tempObject.setVelY(-5);
+                }
+                if(key == KeyEvent.VK_LEFT) {
+                    tempObject.setVelX(-5);
+                }
+                if(key == KeyEvent.VK_DOWN) {
+                    tempObject.setVelY(5);
+                }
+                if(key == KeyEvent.VK_RIGHT) {
+                    tempObject.setVelX(5);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        int key = e.getKeyCode();
+//        super.keyReleased(e);
+        for (int i = 0; i < handler.objects.size(); i++){
+            GameObject tempObject = handler.objects.get(i);
+
+            if(tempObject.getId() == ID.Player){
+                // key event for player1
+                if(key == KeyEvent.VK_W) {
+                    tempObject.setVelY(0);
+                }
+                if(key == KeyEvent.VK_A) {
+                    tempObject.setVelX(0);
+                }
+                if(key == KeyEvent.VK_S) {
+                    tempObject.setVelY(0);
+                }
+                if(key == KeyEvent.VK_D) {
+                    tempObject.setVelX(0);
+                }
+            }
+            if(tempObject.getId() == ID.Player2){
+                if(key == KeyEvent.VK_UP) {
+                    tempObject.setVelY(0);
+                }
+                if(key == KeyEvent.VK_LEFT) {
+                    tempObject.setVelX(0);
+                }
+                if(key == KeyEvent.VK_DOWN) {
+                    tempObject.setVelY(0);
+                }
+                if(key == KeyEvent.VK_RIGHT) {
+                    tempObject.setVelX(0);
+                }
+            }
+        }
+
+    }
+}
---
+++ src/Player.java
@@ -0,0 +1,28 @@
+import java.awt.*;
+import java.util.Random;
+
+public class Player extends GameObject {
+    Random rnd = new Random();
+
+    public Player(int x, int y, ID id){
+        super(x, y, id);
+//        velX = rnd.nextInt(10);
+//        velY = rnd.nextInt(10)+1;
+    }
+
+
+    @Override
+    public void tick() {
+        x += velX;
+        y += velY;
+
+    }
+
+    @Override
+    public void render(Graphics g) {
+        if (id == ID.Player) g.setColor(Color.WHITE);
+        if (id == ID.Player2) g.setColor(Color.BLUE);
+        g.fillRect(x,y,32,32);
+    }
+
+}
