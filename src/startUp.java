60e221a1f1669abb2bb834e1d306cdd4e879f512
Xiyun Zhang <zhxyun201709@163.com>
2019-01-26 23:47:38 -0500

    Merge branch 'master' of /Users/xiyunzhang/Desktop/RiskGame with conflicts.

--- src/startUp.java
+++ src/startUp.java
@@ -14,20 +14,20 @@ public class startUp {
             if (input.equals("[Map]")){
                 author = reader.readLine().trim();
                 mapname = reader.readLine().trim();
-                if (reader.readLine().trim().equals("[Continents]")){
+                if (reader.readLine().trim() == "[Continents]"){
                     int numberOfContinents = Integer.parseInt(reader.readLine().trim());
+
                     for(int i = 0; i< numberOfContinents; i++){
-                        // do something with this info;
-                        // here create continents
-                        reader.readLine();
+
+
+
                     }
-                    if (reader.readLine().equals("[Territories]")) {
+                    if (reader.readLine() == "[Territories]") {
                         int numberOfTerritories = Integer.parseInt(reader.readLine().trim());
                         for(int i = 0; i<numberOfTerritories ; i++){
                             input = reader.readLine();
                             String[] parts = input.split(" ");
                             handler.addObject(new Country(Integer.parseInt(parts[1]), Integer.parseInt(parts[2]), ID.Territory, parts[0]));
-
                         }
                     } else {
                         System.err.println("Wrong Format3");
