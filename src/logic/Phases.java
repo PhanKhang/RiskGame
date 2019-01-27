b09e6670db8de353907526ff7e4c3f932dcb8687
Xiyun Zhang <zhxyun201709@163.com>
2019-01-26 23:43:17 -0500

    country assignment func done

--- src/logic/Phases.java
+++ src/logic/Phases.java
@@ -1,41 +1,66 @@
 package logic;
+import java.util.Collections;
+import java.util.ArrayList;
 
 import logic.Node;
 
 
 public class Phases {
     private int numOfPlayers;
-    private Player[] players;
-    private Node[] graph;
-    private Continent[] worldmap;
+    private ArrayList<Player> players;
+    private ArrayList<Node> graph;
+    private ArrayList<Continent> worldmap;
     //get player input from event handler
     private boolean gaming = false;
 
 
 
-    public Phases( Node[] graph, Continent[] worldmap){
-//        this.numOfPlayers = numOfPlayers;
-        this.players = new Player[this.numOfPlayers];
+    public Phases(ArrayList<Node> graph, ArrayList<Continent> worldmap){
+
         this.graph = graph;
         this.worldmap = worldmap;
 
+    }
+
+    //update players with countries randomly assigned to them. Divide countries as evenly as possible
+    //also for each assigned country put one army of its owner to that country
+    private void countryAssignment(){
+        Collections.shuffle(this.graph);
+        int turnReference = 0;
+        int turn = 0;
+        for(Node node : this.graph){
+            Player player = players.get(turn);
+            player.realms.add(node);
+            node.setOwner(player);
+            turnReference++;
+            turn = turnReference % players.size();
+
+        }
+        //here put a function to pass 'players' and 'graph' to GUI to show how the countries are assigned
+
 
 
     }
-
-
-    private void countryAssignment(){}//update players with countries randomly assigned to them. Divide countries as evenly as possible
-    //also for each assigned country put one army of its owner to that country
     private void determineOrder(){//determine the round robin order for players by changing the order in the players field
 
     }
-
-    private void init(int numOfPlayers){
+//after players type in the number of players and each player's name, call init() to construct the game
+    public void init(int numOfPlayers){
+        if(numOfPlayers == 0){
+            System.out.println("number of players cant be zero");
+        }
         this.numOfPlayers = numOfPlayers;
-        countryAssignment();
+
+        this.players = new ArrayList<>();
+        for(int i = 0; i < numOfPlayers; i++){
+            this.players.add(new Player(i));//i is the player id
+        }
         determineOrder();
+        countryAssignment();
+
         //here put a function returning to GUI to print the assignment result and prompt players
         //pass assignment info(which player owns what countries, player order)
+
 
         gamestart();
 
@@ -43,11 +68,12 @@ public class Phases {
 
     }
     private void gamestart(){
+
         int turnReference = 0;
         int turn = 0;
         Player winner;
         while(true){//keep looping until there is a winner
-            Player current_player = players[turn];
+            Player current_player = players.get(turn);
             phase1(current_player);
             phase2(current_player);
             if(checkGame()){
@@ -63,7 +89,7 @@ public class Phases {
 
 
         }
-        System.out.println(winner.name + " is the winner!");//something to return to GUI to tell who wins
+        System.out.println(winner.id + " is the winner!");//something to return to GUI to tell who wins
     }
     public void passPlayerInput(){
 
