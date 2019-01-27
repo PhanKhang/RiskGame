9b581890ea41839f5e1576e65a813846062d6e74
Xiyun Zhang <zhxyun201709@163.com>
2019-01-26 20:20:19 -0500

    logic thoughts

--- src/logic/Continent.java
+++ src/logic/Continent.java
@@ -1,16 +1,7 @@
 package logic;
-import java.util.HashMap;
-public class WorldMap {
-    HashMap<Continent, Node> worldmap;
-    public WorldMap(){
-
-    }
-
-
-
-}
-class Continent{
+public class Continent{
     String name;
     Node[] countries;
     int control_value;
 }
+
--- src/logic/Node.java
+++ src/logic/Node.java
@@ -1,10 +1,30 @@
 package logic;
 
 import java.util.LinkedList;
-public class Node {
+public class Node {// a Node is a country
     private String name;
     private LinkedList<Node> neighbours;
     private Continent cont;
+    private Player owner;
+    private int current_armies;
+
+// parse txt into Node objects
+    public Node(String name, LinkedList<Node> neighbours, Continent cont){
+        this.name = name;
+        this.neighbours = neighbours;
+        this.cont = cont;
+
+    }
+
+    public void addNeighbour(Node neighbour){
+        this.neighbours.add(neighbour);
+    }
+
+
+
+
+
+
 
 
 
