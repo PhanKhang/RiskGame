c5b4fd4f5ce5ffc28da9586f172ca196ce709cfb
JiaHui-Wang <wjhmars@126.com>
2019-01-27 19:15:44 -0500

    Continent

--- src/Continent.java
+++ src/Continent.java
@@ -1,10 +1,40 @@
+import java.awt.*;
 
-public class Continent{
+public class Continent extends GameObject{
     String name;
     Country[] countries;
     int control_value;
-    public Continent(String name, int control_value){
+    public Continent(int x, int y, ID id, String name, int control_value){
+        super(x, y, id);
         this.name = name;
         this.control_value = control_value;
+    }
+    @Override
+    public void tick() {
+
+    }
+
+    @Override
+    public void render(Graphics g) {
+    }
+
+    @Override
+    public int getValue() {
+        return 0;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public void clicked() {
+
+    }
+
+    @Override
+    public void released() {
+
     }
 }
